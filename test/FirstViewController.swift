4e5bcfb92e7c89efed150901294a6fd68c24e0df
jackrex <jackrex1993@gmail.com>
2019-04-04 11:07:53 +0800

    Initial Commit

---
+++ test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  test
+//
+//  Created by jackrex on 2019/4/4.
+//  Copyright © 2019 Thanos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  test
+//
+//  Created by jackrex on 2019/4/4.
+//  Copyright © 2019 Thanos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
