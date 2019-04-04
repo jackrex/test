1b9667377b00a21668f8b737c7c72b16a300ff53
jackrex <jackrex1993@gmail.com>
2019-04-04 11:24:27 +0800

    init

---
+++ test/Common.swift
@@ -0,0 +1,20 @@
+//
+//  Common.swift
+//  test
+//
+//  Created by jackrex on 2019/4/4.
+//  Copyright © 2019 Thanos. All rights reserved.
+//
+
+import Foundation
+
+class Common: NSObject {
+    static let squareFormat = "?imageMogr2/thumbnail/400x400/format/webp/quality/90"
+    static let smallAvatarFormat = "?imageMogr2/thumbnail/!100x100r"
+    static let largeAvatarFormat = "?imageMogr2/thumbnail/!200x200r"
+    static let timelineSinglePhotoFormat = "imageMogr2/thumbnail/1080x1080/format/webp/quality/90/!fromNative"
+    static let timelineSplitPhotoFormat = "imageMogr2/thumbnail/500x500/format/webp/quality/90/!fromNative"
+    
+    
+    
+}
